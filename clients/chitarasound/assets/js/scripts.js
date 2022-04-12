9ff4183eee4da623dd776a5b77f41753e7e29c27
Joao Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2022-04-12 14:18:49 +0200

    Chitara sound

---
+++ clients/chitarasound/assets/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    // alert('Works!')
+});
+
+function service(evt, serviceName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(serviceName).style.display = "block";
+    evt.currentTarget.className += " active";
+};
